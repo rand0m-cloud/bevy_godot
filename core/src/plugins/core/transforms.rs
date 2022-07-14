65e0002677b2a4ebbe761157103771127d1aebc8
Abby Bryant <rand0m-cloud@outlook.com>
2022-07-14 15:21:23 -0400

    fix(transform): don't open mutable borrow if transforms are equal

--- core/src/plugins/core/transforms.rs
+++ core/src/plugins/core/transforms.rs
@@ -27,7 +27,9 @@ fn pre_update_godot_transforms(
     mut entities: Query<(&mut Transform, &mut ErasedGodotRef)>,
 ) {
     for (mut transform, mut reference) in entities.iter_mut() {
-        let obj = reference.get::<Spatial>();
-        *transform = obj.transform().to_bevy_transform();
+        let godot_transform = reference.get::<Spatial>().transform().to_bevy_transform();
+        if *transform != godot_transform {
+            *transform = godot_transform;
+        }
     }
 }
