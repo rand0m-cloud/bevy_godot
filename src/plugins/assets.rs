04cc76d4c4d4f0696bb994e4c7fe58008e01c2a0
Abby Bryant <rand0m-cloud@outlook.com>
2022-08-27 23:10:33 -0400

    fix: add an AssetIo implementation to support exported games

--- src/plugins/assets.rs
+++ src/plugins/assets.rs
@@ -1,11 +1,18 @@
 use crate::prelude::{
+    bevy_prelude::*,
     godot_prelude::{GodotError, RefCounted, SubClass, ThreadLocal},
     *,
 };
-use bevy::asset::{AssetLoader, AssetPlugin, AssetServerSettings, LoadContext, LoadedAsset};
+use bevy::asset::*;
 use bevy::reflect::TypeUuid;
 use bevy::utils::BoxedFuture;
-use std::time::Duration;
+use std::{
+    future::Future,
+    io::{Error, ErrorKind},
+    path::{Path, PathBuf},
+    pin::Pin,
+    time::Duration,
+};
 
 pub struct GodotAssetsPlugin;
 impl Plugin for GodotAssetsPlugin {
@@ -18,6 +25,7 @@ impl Plugin for GodotAssetsPlugin {
                 .to_string(),
             watch_for_changes: false,
         })
+        .insert_resource(AssetServer::with_boxed_io(Box::new(GodotAssetIo)))
         .add_plugin(AssetPlugin)
         .add_asset::<GodotResource>()
         .add_asset::<ErasedGodotRef>()
@@ -37,6 +45,98 @@ impl GodotResource {
         &mut self,
     ) -> Option<Ref<T, ThreadLocal>> {
         unsafe { self.0.clone().assume_thread_local().cast() }
+    }
+}
+
+fn make_godot_path(path: &Path) -> String {
+    format!("res://{}", path.to_str().expect("path to be valid unicode"))
+}
+
+trait GodotErrorExt<T> {
+    fn map_godot_error(self) -> Result<T, AssetIoError>;
+}
+
+impl<T> GodotErrorExt<T> for Result<T, GodotError> {
+    fn map_godot_error(self) -> Result<T, AssetIoError> {
+        self.map_err(|e| AssetIoError::Io(Error::new(ErrorKind::Other, e.to_string())))
+    }
+}
+
+pub struct GodotAssetIo;
+impl AssetIo for GodotAssetIo {
+    fn load_path<'a>(
+        &'a self,
+        path: &'a Path,
+    ) -> Pin<Box<dyn Future<Output = Result<Vec<u8>, AssetIoError>> + Send + 'a>> {
+        Box::pin(async {
+            let godot_path = make_godot_path(path);
+
+            let resource_loader = ResourceLoader::godot_singleton();
+            if !resource_loader.exists(godot_path, "") {
+                return Err(AssetIoError::NotFound(path.to_path_buf()));
+            }
+
+            Ok(vec![])
+        })
+    }
+    fn read_directory(
+        &self,
+        path: &Path,
+    ) -> Result<Box<dyn Iterator<Item = PathBuf> + 'static>, AssetIoError> {
+        let godot_path = make_godot_path(path);
+
+        let mut entries = vec![];
+
+        let dir = Directory::new();
+        dir.open(godot_path).map_godot_error()?;
+        dir.list_dir_begin(true, false).map_godot_error()?;
+
+        let mut file;
+
+        loop {
+            file = dir.get_next();
+            let file = file.to_string();
+            if file == "" {
+                break;
+            }
+            entries.push(PathBuf::from(file));
+        }
+
+        Ok(Box::new(entries.into_iter()))
+    }
+    fn get_metadata(&self, path: &Path) -> Result<Metadata, AssetIoError> {
+        let godot_path = make_godot_path(path);
+
+        let dir = Directory::new();
+        let entry_type = if dir.dir_exists(&godot_path) {
+            FileType::Directory
+        } else if dir.file_exists(&godot_path) {
+            FileType::File
+        } else {
+            return Err(AssetIoError::Io(Error::new(
+                ErrorKind::Other,
+                "path is not a directory or a file",
+            )));
+        };
+
+        Ok(Metadata::new(entry_type))
+    }
+    fn watch_path_for_changes(&self, _path: &Path) -> Result<(), AssetIoError> {
+        Ok(())
+    }
+    fn watch_for_changes(&self) -> Result<(), AssetIoError> {
+        Ok(())
+    }
+
+    fn is_dir(&self, path: &Path) -> bool {
+        let godot_path = make_godot_path(path);
+        let dir = Directory::new();
+        dir.dir_exists(godot_path)
+    }
+    fn is_file(&self, path: &Path) -> bool {
+        let godot_path = make_godot_path(path);
+        let dir = Directory::new();
+        dir.file_exists(godot_path)
     }
 }
 
