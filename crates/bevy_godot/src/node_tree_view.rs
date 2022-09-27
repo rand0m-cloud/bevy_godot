077beef0c9dfc6590fdde9525d77786244ed1a7d
Abby Bryant <rand0m-cloud@outlook.com>
2022-09-27 00:32:23 -0400

    improve NodeTreeView to accept generic TRefs

--- crates/bevy_godot/src/node_tree_view.rs
+++ crates/bevy_godot/src/node_tree_view.rs
@@ -1,5 +1,5 @@
-use crate::prelude::Node;
+use crate::prelude::{godot_prelude::SubClass, Node, TRef};
 
 pub trait NodeTreeView {
-    fn from_node(node: &Node) -> Self;
+    fn from_node<T: SubClass<Node>>(node: TRef<T>) -> Self;
 }
