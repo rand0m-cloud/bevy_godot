b3024e44396dab8a8b83982b259268995d24d877
Abby Bryant <rand0m-cloud@outlook.com>
2022-10-25 18:20:52 -0400

    add error message to godot runner to help users debug the issue
    Fixes #12

--- examples/run_godot.rs
+++ examples/run_godot.rs
@@ -2,11 +2,16 @@ use std::process::{Command, Stdio};
 
 fn main() {
     println!(env!("CARGO_MANIFEST_DIR"));
+
+    let run_dir = format!("{}/godot", env!("CARGO_MANIFEST_DIR"));
+
     Command::new("godot")
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .current_dir(format!("{}/godot", env!("CARGO_MANIFEST_DIR")))
-        .output()
-        .unwrap();
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .current_dir(&run_dir)
+            .output()
+            .unwrap_or_else(|_| {
+                panic!("tried running `godot` in {}. try adding a Godot Editor executable to your path and ensuring that the godot directory exists.", run_dir)
+            });
 }
